44510dadb3f4bd673d110c8b60d230b5eb621924
Marcin Biernat <mb@marcinbiernat.pl>
2016-06-25 16:48:49 +0200

    flycheck-mypy

--- personal/personal.el
+++ personal/personal.el
@@ -1,4 +1,14 @@
 (global-hl-line-mode 0)
 
-(prelude-require-package 'yasnippet)
+(prelude-require-packages
+ '(
+   flycheck-mypy
+   yasnippet
+   )
+ )
+
 (yas-global-mode +1)
+
+(require 'flycheck-mypy)
+
+(flycheck-add-next-checker 'python-flake8' (warning . python-mypy))
