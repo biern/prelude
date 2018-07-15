780529b40e76f2081d56745c1d51d367cd57ed86
Marcin Biernat <mb@marcinbiernat.pl>
2018-07-15 14:37:46 +0200

    Typescript web mode and custom key bindings

--- personal/tide.el
+++ personal/tide.el
@@ -6,15 +6,38 @@
   (flycheck-add-next-checker 'typescript-tide '(t . typescript-tslint) 'append)
   (eldoc-mode +1)
   (tide-hl-identifier-mode +1)
+  (personal-setup-tide-keys)
   ;; company is an optional dependency. You have to
   ;; install it separately via package-install
   ;; `M-x package-install [ret] company`
   (company-mode +1))
 
+
+(defun personal-setup-tide-keys ()
+  (local-set-key (concat (kbd "C-c SPC") "s") 'tide-rename-symbol)
+  (local-set-key (concat (kbd "C-c SPC") "f") 'tide-fix)
+  (local-set-key (concat (kbd "C-c SPC") "u") 'tide-references)
+  (local-set-key (concat (kbd "C-c SPC") "i") 'tide-format)
+  )
+
+
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
 
-;; ;; formats the buffer before saving
+;; formats the buffer before saving
 ;; (add-hook 'before-save-hook 'tide-format-before-save)
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;; TSX
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setq web-mode-markup-indent-offset 2)
+              (setup-tide-mode))))
+
+;; enable typescript-tslint checker
+(flycheck-add-mode 'typescript-tslint 'web-mode)
